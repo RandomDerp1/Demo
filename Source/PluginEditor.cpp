98e7c14c156a163d6b86297ad2996fd8f7ed219f
Jove De Keersmaeker <jove0125@gmail.com>
2023-04-20 09:24:05 +0200

    changed the message

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -30,7 +30,7 @@ void DemoAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("Hello ISP!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void DemoAudioProcessorEditor::resized()
